cceeaec52202a7bf9ebe8ad51438e232a4e25a60
Binay <binaykumarrana@gmail.com>
2020-06-08 12:57:21 +0800

    Adding changes

--- src/main/java/com/sms/demo/SmsApplication.java
+++ src/main/java/com/sms/demo/SmsApplication.java
@@ -3,10 +3,13 @@ package com.sms.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+* Springboot application class
+* */
 @SpringBootApplication
-public class DemoApplication {
+public class SmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SmsApplication.class, args);
 	}
 }
--- src/main/java/com/sms/demo/SmsController.java
+++ src/main/java/com/sms/demo/SmsController.java
@@ -3,15 +3,22 @@ package com.sms.demo;
 import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * SmsController class to represent the class of creating the http request '/sms'
+ * Output will be response that will be passed after the http request executed.
+ *
+ * Response type 'application/xml'
+*/
 @RestController
-public class WebhookController {
+public class SmsController {
 
     @RequestMapping(value = "/sms")
     @ResponseBody
-    public String getTwiML() {
+    public String getTwiMLResponse() {
         Body body = new Body
                 .Builder("Thank you for the message!")
                 .build();
@@ -27,7 +34,7 @@ public class WebhookController {
     }
 
     @RequestMapping("/")
-    public String getHello(){
-        return "Hello";
+    public String getLanding() {
+        return "Welcome to lading page for SMS! Use /sms for webook to trigger.";
     }
 }
